103327f877770dbb36391e5d20bcbb92f58524ba
madzia89 <wojcicka.ma@gmail.com>
2018-05-14 11:25:19 +0200

    run redux dev tools

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {store} from './store'
 
 class App extends Component {
   render() {
--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import {createStore, combineReducers} from 'redux'
 import counterReducer from './state/counter'
 
-const reducer = combneReducers({
-    counter: counterReducer
+const reducer = combineReducers({
+    counter: counterReducer,
 })
 
-const store = createStore (reducer)
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
