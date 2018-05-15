2db9091dd3e649d4c2101ade72e5f2ecba6a1515
madzia89 <wojcicka.ma@gmail.com>
2018-05-15 14:25:06 +0200

    add onClick

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,38 +1,55 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import TextField from 'material-ui/TextField'
-import {add, del} from "../state/todos"
+import {add, del, newTaskText} from "../state/todos"
 import RaisedButton from 'material-ui/RaisedButton'
 
 const ToDo = (props) => (
     <div>
-        <ul>
-            {props.tasks.map((task, i) => (
-                <li key={i}>
-                    {task.text}
-                    {<RaisedButton
-                        name={'deleteTask'}
-                        onClick={() => props.remove(i)}
-                        label={'delete'}
-                        primary={true}
-                    />}
-                </li>
-            ))}
+        <div>
+            <TextField
+                name={'addTasks'}
+                onChange={props.onNewTaskChange}
+                value={props.newTaskText}
 
-        </ul>
-        <TextField
-            name={'addTasks'}
-        />
+            />
+            <RaisedButton
+                name={'addText'}
+                label={'ADD'}
+                onClick={() => props.addValue()}
+            />
+        </div>
+        <div>
+            <ul>
+                {props.tasks.map((task, i) => (
+                    <li key={i}>
+                        {task.text}
+                        {<RaisedButton
+                            name={'deleteTask'}
+                            //poniżej wywpłujemy funkcję z eventem czyli (), to oznacza ze ta funkca wywoła sę tylko z eventem
+                            // czyli kliknięciem, bez tegonie ma eventu więc funkcja nie zadziała,
+                            // w tym zapisie kliknięcie uruchamia funkcję która wywoła funkcję z indexem
+                            onClick={() => props.remove(i)}
+                            label={'delete'}
+                            primary={true}
+                        />}
+                    </li>
+                ))}
 
+            </ul>
+        </div>
     </div>
 )
 
 const mapStateToProps = state => ({
-    tasks: state.todos
+    tasks: state.todos.tasks,
+    newTaskText: state.todos.newTaskText
 })
 
 const mapDispatchToProps = dispatch => ({
-    remove: (index) => dispatch(del(index))
+    remove: (index) => dispatch(del(index)),
+    addValue: () => dispatch(add()),
+    onNewTaskChange: (event, value) => dispatch(newTaskText(value))
 })
 
 export default connect(
--- src/state/todos.js
+++ src/state/todos.js
@@ -1,23 +1,42 @@
 const ADD = 'todos/ADD'
 const DEL = 'todos/DELETE'
+const NEW_TASK_TEXT = 'todos/NEW_TASK_TEXT'
 
-export const add = (taskText) => ({type: ADD, taskText})
+export const add = () => ({type: ADD})
 export const del = (index) => ({type: DEL, index}) //index bo będziemy usuwać po indexie
+export const newTaskText = (newText) => ({type: NEW_TASK_TEXT, newText}) //index bo będziemy usuwać po indexie
 
 
-const initialState = []
+const initialState = {
+    tasks: [],
+    newTaskText: ''
+}
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case NEW_TASK_TEXT:
+            return {
+                    ...state,
+                    newTaskText: action.newText
+                }
         case ADD:
-            return action.taskText ?  //to mówi, że jeżeli action.taskText istnieje to ma zrobić concata, a jeżeli nie to ma zwrócić stan
-                state.concat({ //zwracamy nową tablicę łączącą stan z taskText i właściwością completed
-                    text: action.taskText,
-                    completed: false
-                })
+            return state.newTaskText ?  //to mówi, że jeżeli action.taskText istnieje to ma zrobić concata, a jeżeli nie to ma zwrócić stan
+                {
+                    ...state,
+                    newTaskText: '',
+                    tasks: state.tasks.concat({
+                        text: state.newTaskText,
+                        completed: false
+                    })
+                }
                 : state
+
         case DEL:
-            return state.filter((task, index) => index !== action.index)
+            return {
+                ...state,
+                tasks:
+                    state.tasks.filter((task, index) => index !== action.index)
+            }
         default:
             return state
 
