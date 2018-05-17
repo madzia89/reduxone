737d4ade4d11c84bbe96c59662625e0e478f95c8
madzia89 <wojcicka.ma@gmail.com>
2018-05-17 09:45:00 +0200

    init for logging

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import LogInByGoogle from "./LogInByGoogle";
+import { logInByGoogle } from '../../state/auth'
 
 const Auth = (props) => (
     <div>
-
+        {//jeżeli użytkownik jest zalogowany to mają być wyświetlone dzieci propsa
+            //jeżeli nie jest zalogowany ma się wyświetlić component LogInByGoogle
+            props.isUserLoggedIn ?
+                props.children
+                :
+                <LogInByGoogle
+                    onLogInHandler={}
+                />
+        }
     </div>
 )
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+    isUserLoggedIn: state.auth.isUserLoggedIn
+})
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+    logInByGoogle: () => dispatch(logInByGoogle())
+})
 
 export default connect(
     mapStateToProps,
--- src/store.js
+++ src/store.js
@@ -1,8 +1,7 @@
 import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
-import counter, {/*inc, dec*/} from './state/counter'
-import todos, {/*add, del*/} from './state/todos'
+import counter from './state/counter'
+import todos from './state/todos'
 import exerciseState from './state/exerciseState'
-// import counter2, {inc as incCounter2, dec as decCounter2} from './state/counter2'
 import randomUsers, {setUsersList, fetchUsers} from './state/randomUsers'
 import thunk from 'redux-thunk'
 import asyncReduxCounter, {initCounterSync} from './state/asyncReduxCounter'
@@ -15,30 +14,17 @@ export const reducer = combineReducers({
     asyncReduxCounter,
 })
 
-//dzięki poniższemu thunk będzie współgrał z devtoolsami
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const store = createStore(
     reducer,
-    composeEnhancers(//compose składa dodatki do reakta
+    composeEnhancers(
         applyMiddleware(thunk)
     )
 )
 
 store.dispatch(initCounterSync())
 
-
-//
-// //metoda do dispatchowania
-// //dispatch jest tym co przyjmuje akcję i przekazuje je do reducerów
-// window.inc = (value) => store.dispatch(inc(value)) //musimy tutaj wywołać inc, bo inc jest action creator który zwraca akcję a my właśnie chcemy akcję a nie jego kreatora
-// window.dec = (value) => store.dispatch(dec(value))
-//
-// window.addTask = (taskText) => store.dispatch(add(taskText))
-// window.delTask = (index) => store.dispatch(del(index))
-//
-//
-// // store.dispatch(incCounter2())
-// // store.dispatch(decCounter2())
 
 
